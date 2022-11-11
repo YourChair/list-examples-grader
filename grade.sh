ef0fdeb5ac4c202ed230e956e749f4b5db5fe2fd
YourChair <asieh@ucsd.edu>
2022-11-11 11:07:43 -0800

    a

--- grade.sh
+++ grade.sh
@@ -5,3 +5,30 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+
+
+if (-f "ListExamples.java")
+    then
+    echo "File exists"
+    mkdir gradingDir
+    cp student-submission gradingDir
+    cp TestListExamples.java gradingDir
+    cd gradingDir
+    unset -e
+    javac ".\*.java"
+    java ListExamples
+    stderr abc
+
+    #run tests
+    if ($? != 0)
+        then
+        echo "tests do not pass"
+    
+    else
+        echo "the tests pass"
+    fi
+else
+    echo "File not found"
+    exit
+fi
+
